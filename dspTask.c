25e82b1a14f86fd0f1b0f046d923fad9b6aa13d0
gitfiq <128337912+gitfiq@users.noreply.github.com>
2023-07-22 19:11:05 +0800

    Update dspTask.c

--- dspTask.c
+++ dspTask.c
@@ -13,13 +13,15 @@ unsigned int adc_GetTemp(void);
 unsigned int adc_GetOxy(void);
 unsigned int get_pHcnt(void);
 
-void seg_DspAll(unsigned int Tempcnt);
+//void seg_DspAll(unsigned int Tempcnt);
+void getTemp_count(unsigned int gettempcnt);
 void lcd_DspAll(void);
 
 void simulate_SetLabel(void);
 void simulate_Reoxygenation(void);
 void simulate_LowpH(void);
 void simulate_HighpH(void);
+unsigned char get_emergencyState(void);
 
 unsigned int Tempcnt = 0;
 unsigned int Oxycnt = 0;
@@ -32,17 +34,18 @@ void dspTask_OnLCD(void){
     simulate_SetLabel();
     
     lcd_DspAll();
-    
-    if(Oxycnt < 5){
-        simulate_Reoxygenation();
-    }  
-    if(pHcnt < 6){
-        simulate_LowpH();
-        pHcnt = 7;
-    }
-    else if(pHcnt > 8){
-        simulate_HighpH();
-        pHcnt = 7;
+    if(get_emergencyState() == 0){
+        if(Oxycnt < 5){
+            simulate_Reoxygenation();
+        }  
+        if(pHcnt < 6){
+            simulate_LowpH();
+            pHcnt = 7;
+        }
+        else if(pHcnt > 8){
+            simulate_HighpH();
+            pHcnt = 7;
+        }
     }
   
 }
@@ -50,6 +53,5 @@ void dspTask_OnLCD(void){
 void dspTask_OnSeg(void){
     Tempcnt = adc_GetTemp();
     
-        seg_DspAll(Tempcnt);
-
+    getTemp_count(Tempcnt);
 }
