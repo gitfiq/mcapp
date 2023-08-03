889bd501347da1ddf3a776a91c686a75d978ee59
gitfiq <128337912+gitfiq@users.noreply.github.com>
2023-08-03 11:11:40 +0800

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include <xc.h>
 #include "config.h"
 
+//Initialize the system
 void initSysPins(void);
 void initSysTimer(void);
 void initSysIOC(void);
@@ -17,12 +18,17 @@ void initCCP(void);
 void initADC(void);
 void initLCD(void);
 
+//Start the ADC conversion
 void adc_GetConversion(void);
 
+//Display the relevant values(Temperature, pH, Oxygen level, Hours) on the LCD and seven Segment
 void dspTask_OnLCD(void);
 void dspTask_OnSeg(void);
 
+//Continuously call for motor to on at equal periods
 void scheduled_Motor(void);
+
+//Call function to allow polling of the pH value(Increase or decrease)
 void usrTask(void);
 
 void main(void) {
@@ -40,9 +46,9 @@ void main(void) {
         
         dspTask_OnLCD();
         dspTask_OnSeg();
-
+        
         usrTask();
-        //scheduled_Motor();
+        scheduled_Motor();
     }
     return;
 }
@@ -54,9 +60,9 @@ void initSysPins(void){
     ANSELD = 0b00000000;
     ANSELE = 0b00000000;
     
-    TRISA = 0b11110111;
+    TRISA = 0b11111111;
     TRISB = 0b11000000;
     TRISC = 0b00000000;
     TRISD = 0b11000000;
-    TRISE = 0b11111100;
+    TRISE = 0b11101000;
 }
